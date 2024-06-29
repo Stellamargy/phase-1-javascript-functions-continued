e42c9c83a44fb4349006953ed95b304bda415fbe
stlmargy <stellahmargy0211@gmail.com>
2024-06-30 00:24:16 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+// console.log(saturdayFun())
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+const wrapAdjective = function (initializer) {
+  return function result(statement) {
+    return `You are ${initializer}${statement}${initializer}!`
+    
+  };
+};
+// console.log(wrapAdjective("@")("a good girl"))
